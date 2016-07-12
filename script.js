48969fc9b1b8aababb96997c5313e6c7991804bf
Gokul K <gokulk@qburst.com>
2016-07-12 09:15:27 +0530

    modified script

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.body.addEventListener('mouseup', function(e) {
-  var popup = document.querySelector('.highlightMenu');
+  var highlightMenu = document.querySelector('.highlightMenu');
   if (window.getSelection().toString().length > 0) {
     var selectedText = window.getSelection();
     var textCoordinates = selectedText.getRangeAt(0).getBoundingClientRect();
-    popup.classList.toggle('highlightMenu--active');
-    popup.style.top = (textCoordinates.top - popup.getBoundingClientRect().height - 8) + 'px';
-    popup.style.left = (textCoordinates.left + textCoordinates.width / 2 - popup.getBoundingClientRect().width / 2) + 'px';
+    highlightMenu.classList.toggle('highlightMenu--active');
+    highlightMenu.style.top = (textCoordinates.top - highlightMenu.getBoundingClientRect().height - 8) + 'px';
+    highlightMenu.style.left = (textCoordinates.left + textCoordinates.width / 2 - highlightMenu.getBoundingClientRect().width / 2) + 'px';
   } else {
-    popup.classList.remove('highlightMenu--active');
+    highlightMenu.classList.remove('highlightMenu--active');
   }
 });
 window.addEventListener('mousedown', function() {
