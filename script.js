d5eea3bfc45f5bf8e258cf95cf7be16f1d785d96
Gokul K <gokulk@qburst.com>
2017-05-06 01:57:19 +0530

    add preventDefault for prohibited events

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-document.body.addEventListener('mouseup', function(e) {
-  var highlightMenu = document.querySelector('.highlightMenu');
-  if (window.getSelection().toString().length > 0 && $(e.target).parents('.highlightMenu').length === 0) {
-    var selectedText = window.getSelection();
-    var textCoordinates = selectedText.getRangeAt(0).getBoundingClientRect();
-    highlightMenu.classList.toggle('highlightMenu--active');
-    highlightMenu.style.top = (textCoordinates.top - highlightMenu.getBoundingClientRect().height - 8 + window.scrollY) + 'px';
-    highlightMenu.style.left = (textCoordinates.left + textCoordinates.width / 2 - highlightMenu.getBoundingClientRect().width / 2) + 'px';
-  } else {
-    // highlightMenu.classList.remove('highlightMenu--active');
+document.body.addEventListener('mouseup', e => {
+  const hltMenu = document.getElementsByClassName('hltMenu')[0];
+  if (window.getSelection().toString().length > 0 && e.target.tagName !== 'BUTTON' && !e.target.className.includes('hlt')) {
+    const selectedText = window.getSelection();
+    const textCoordinates = selectedText.getRangeAt(0).getBoundingClientRect();
+    hltMenu.classList.toggle('hltMenu--active');
+    hltMenu.style.top =`${textCoordinates.top - hltMenu.getBoundingClientRect().height - 8 + window.scrollY}px`;
+    hltMenu.style.left =`${textCoordinates.left + textCoordinates.width / 2 - hltMenu.getBoundingClientRect().width / 2}px`;
   }
-},false);
+}, false);
 
-window.addEventListener('mousedown', function(e) {
-  if($(e.target).parents('.highlightMenu').length === 0) {
-    document.querySelector('.highlightMenu').classList.remove('highlightMenu--active');
+window.addEventListener('mousedown', e => {
+  if (e.target.tagName !== 'BUTTON' && !e.target.className.includes('hlt')) {
+    document.getElementsByClassName('hltMenu')[0].classList.remove('hltMenu--active');
+  } else {
+    e.preventDefault();
   }
-},false);
+}, false);
